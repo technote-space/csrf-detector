2d4a311c825a2ef7e3f6c02f0775d79277470bb3
Technote <technote.space@gmail.com>
2020-12-13 02:16:33 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/csrf-detector
  * Description: This plugin will detect csrf
  * Author: Technote
- * Version: 0.2.17
+ * Version: 0.2.18
  * Author URI: https://technote.space
  * Text Domain: csrf-detector
  * Domain Path: /languages/
